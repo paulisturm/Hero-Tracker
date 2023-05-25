2cf47f110fd71ecf14dd24671d6d92e668bf7483
paulisturm <paulisturm22@gmail.com>
2023-05-25 18:57:39 -0400

    inventory

--- models/inventory.js
+++ models/inventory.js
@@ -1,2 +1,36 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Inventory extends Model {}
+
+Inventory.init(
+{
+    id: {
+        type: DataTypes.INTEGER,
+        allowsNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    item_name: {
+        type: DataTypes.STRING,
+        allowsNull: false
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowsNull: false
+    },
+    item_weight: {
+        type: DataTypes.INTEGER,
+        allowsNull: false,
+    },
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Inventory;
