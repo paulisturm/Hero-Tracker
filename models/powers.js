4f50375a9ab978eed489402eab5190b1059657c3
paulisturm <paulisturm22@gmail.com>
2023-05-25 19:08:57 -0400

    models

---
+++ models/powers.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Powers extends Model {}
+
+Powers.init(
+   {
+    id: {
+        type: DataTypes.INTEGER,
+        allowsNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      power_name: {
+        type: DataTypes.STRING,
+        allowsNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      power_type: {
+        type: DataTypes.STRING,
+        allowsNull: false
+      },
+      power_damage: {
+        type: DataTypes.INTEGER,
+        allowsNull: false
+      }
+   },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+      }
+);
+
+module.exports = Powers;
--- models/weapons.js
+++ models/weapons.js
@@ -1,6 +1,36 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Inventory extends Model {}
+class Weapons extends Model {}
 
-Inventory.init()
+Weapons.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowsNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        weapon_name: {
+            type: DataTypes.STRING,
+            allowsNull: false
+        },
+        attack_damage: {
+            type: DataTypes.INTEGER,
+            allowsNull: false
+        },
+        attack_speed: {
+            type: DataTypes.INTEGER,
+            allowsNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+      }
+);
+
+module.exports = Weapons;
