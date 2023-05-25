f80ae93d2617dd87e5b8585da9735838c21cf197
paulisturm <paulisturm22@gmail.com>
2023-05-25 19:26:23 -0400

    stuff

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+  
