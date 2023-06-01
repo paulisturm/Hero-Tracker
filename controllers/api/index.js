cbc00bbfbb4c68684cdb528450a9f1aded688e87
paulisturm <paulisturm22@gmail.com>
2023-06-01 18:50:00 -0400

    routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const inventoryRoutes = require('./inventory-routes');
 const weaponsRoutes = require('./weapons-routes');
 const powersRoutes = require('./powers-routes');
+const userRoutes = require("./user-routes");
+const itemRoutes = require("./item-routes")
 
 router.use('/inventory', inventoryRoutes);
 router.use('/weapons', weaponsRoutes);
 router.use('/powers', powersRoutes);
+router.use('/user', userRoutes);
+router.use('/item', itemRoutes);
 
 module.exports = router;
