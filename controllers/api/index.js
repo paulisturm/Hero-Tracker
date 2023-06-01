57713431a472941e716f5e409c2b21331918f2a5
Seth <sandregg2gmail.com>
2023-05-31 21:32:03 -0400

    user login stuff -SA

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const inventoryRoutes = require('./inventory-routes');
 const weaponsRoutes = require('./weapons-routes');
 const powersRoutes = require('./powers-routes');
+const userRoutes = require('./user-routes.js');
+
 
 router.use('/inventory', inventoryRoutes);
 router.use('/weapons', weaponsRoutes);
