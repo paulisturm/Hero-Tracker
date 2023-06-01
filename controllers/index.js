a94bc903d54d26e24bd566fecfa8c4d7f89fabd8
paulisturm <paulisturm22@gmail.com>
2023-06-01 19:43:43 -0400

    handle bars

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const pageRoutes = require('./page-routes')
 
 router.use('/api', apiRoutes);
+router.use('/', pageRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
 
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
