14dec397529d6b03e16242ffa666a46c9e952eda
paulisturm <paulisturm22@gmail.com>
2023-06-01 20:00:41 -0400

    stuff

--- controllers/page-routes.js
+++ controllers/page-routes.js
@@ -14,4 +14,28 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/', async (req, res) => {
+    try {
+      
+      res.render('login', {
+  
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/', async (req, res) => {
+    try {
+      
+      res.render('dashboard', {
+  
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
